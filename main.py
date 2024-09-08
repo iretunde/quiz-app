f8ff32ae4632416f9742fe6fc209da9caa7ff8a8
iresoleye <120500584+iresoleye@users.noreply.github.com>
2024-09-08 22:39:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests, html
+from ui import QuizInterface
+response = requests.get(url='https://opentdb.com/api.php?amount=10&category=31&type=boolean')
+response.raise_for_status()
+question_data = response.json()['results']
+print(response.json())
+
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(question_bank)
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
